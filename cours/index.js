924ecc09bb853ba019dcc686c25c695aabc49fbd
ixyhoo <ixyhoo@outlook.fr>
2023-07-02 20:53:49 +0200

    fillStyle

--- cours/index.js
+++ cours/index.js
@@ -14,3 +14,10 @@ ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
 ctx.fillRect(10, 25, 100, 50);
 ctx.clearRect(35, 25, 40, 50);
 ctx.clearRect(125, 75, 50, 50);
+
+ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+ctx.beginPath();
+ctx.moveTo(180, 150);
+ctx.lineTo(100, 75);
+ctx.lineTo(100, 150);
+ctx.fill();
