93db94d0872069e7df78d679cebe28baa6186fff
ixyhoo <ixyhoo@outlook.fr>
2023-07-02 20:57:33 +0200

    draw

--- cours/index.js
+++ cours/index.js
@@ -1,23 +1,27 @@
 //CANVAS
 //-------
+function draw (){
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    
+    ctx.fillStyle = "red";
+    ctx.fillRect(50, 50, 50, 150);
+    
+    ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.fillRect(100, 30, 100, 50);
+    
+    ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+    ctx.fillRect(10, 25, 100, 50);
+    ctx.clearRect(35, 25, 40, 50);
+    ctx.clearRect(125, 75, 50, 50);
+    
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.beginPath();
+    ctx.moveTo(180, 150);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 150);
+    ctx.fill();
+}
 
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 50, 150);
-
-ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-ctx.fillRect(100, 30, 100, 50);
-
-ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
-ctx.fillRect(10, 25, 100, 50);
-ctx.clearRect(35, 25, 40, 50);
-ctx.clearRect(125, 75, 50, 50);
-
-ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-ctx.beginPath();
-ctx.moveTo(180, 150);
-ctx.lineTo(100, 75);
-ctx.lineTo(100, 150);
-ctx.fill();
+window.addEventListener("load", draw);
