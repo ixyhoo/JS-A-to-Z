7a881abaa0e8463d71e21bdce791aab19860714e
ixyhoo <ixyhoo@outlook.fr>
2023-07-02 20:49:51 +0200

    canvas

--- cours/index.js
+++ cours/index.js
@@ -8,4 +8,9 @@ ctx.fillStyle = "red";
 ctx.fillRect(50, 50, 50, 150);
 
 ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-ctx.fillRect(30, 10, 150, 100);
+ctx.fillRect(100, 30, 100, 50);
+
+ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+ctx.fillRect(10, 25, 100, 50);
+ctx.clearRect(35, 25, 40, 50);
+ctx.clearRect(125, 75, 50, 50);
