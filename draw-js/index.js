1f505f46e701a130edaa8f056fb28d3d045bf6f7
ixyhoo <ixyhoo@outlook.fr>
2023-07-03 05:57:04 +0200

    canvas exercise finish

--- draw-js/index.js
+++ draw-js/index.js
@@ -10,13 +10,27 @@ function getMousePos(e) {
    
 }
 
+function mouseMove(e){
+    const mousePos = getMousePos(e);
+    ctx.lineTo(mousePos.x, mousePos.y);
+    ctx.stroke();
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+}
 canvas.addEventListener("mousedown", (e) => {
     e.preventDefault();
     const mousePos = getMousePos(e);
     ctx.beginPath();
     ctx.moveTo(mousePos.x, mousePos.y);
+
+    canvas.addEventListener("mousemove", mouseMove);
+    canvas.addEventListener("mouseup", () => {
+        canvas.removeEventListener("mousemove", mouseMove);
+    });
 });
 
-
+reset.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);                                       
+});
 
 
