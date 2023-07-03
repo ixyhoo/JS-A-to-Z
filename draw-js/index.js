e7d8f58380a4cd12e11e2aebcdfa5990182266df
ixyhoo <ixyhoo@outlook.fr>
2023-07-03 05:44:56 +0200

    getMousePos

--- draw-js/index.js
+++ draw-js/index.js
@@ -1,3 +1,21 @@
+const canvas = document.getElementById("art");
+const ctx = canvas.getContext("2d");
+
+function getMousePos(e) {
+    const rect = canvas.getBoundingClientRect()
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+    };
+   
+}
+
+canvas.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    const mousePos = getMousePos(e);
+    ctx.beginPath();
+    ctx.moveTo(mousePos.x, mousePos.y);
+});
 
 
 
